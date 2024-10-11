1ae024d8078733c58bee4c8db5a08e4958cb3ddb
Carlos Ambeliz <carlosambeliz@gmail.com>
2024-10-11 12:00:21 -0600

    Actualizacion

--- src/Service/Api/api.js
+++ src/Service/Api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'oci-server.ddns.net:3000/api',
+  baseURL: 'https://api.siews.net/api',
   headers: {
     'Content-Type': 'application/json',
   },
